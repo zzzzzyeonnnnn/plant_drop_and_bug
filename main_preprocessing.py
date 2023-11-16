7368683ccec10d73f6e048cf4da41eed3e7ade1a
zzzzzyeonnnnn <seongkke@naver.com>
2023-11-16 12:05:16 +0900

    main_preprocessing.py

--- main_preprocessing.py
+++ main_preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
+import os
 
 def getPrediction(filename):
     #클래스 레이블 정의
@@ -13,10 +14,13 @@ def getPrediction(filename):
         label[idx] = 1
 
     #load model
-    my_model=load_model("model/model_2.h5")
+    #my_model=load_model("model/model_2.h5")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(script_dir, 'model/model_2.h5')
 
     SIZE = 224 #크기 정의
-    img_path = 'static/images/' + filename #이미지 경우, 사용자가 업로드 하는 경로
+    #img_path = 'static/images/' + filename #이미지 경우, 사용자가 업로드 하는 경로
+    img_path = os.path.join(script_dir, 'static/images/', filename)
     img = np.asarray(Image.open(img_path).resize((SIZE, SIZE))) #넘파이 배열로 반환
     img = np.expand_dims(img, axis=0) #치수를 오른쪽으로 확장
 
